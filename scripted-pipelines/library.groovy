c49c3bb6208549822de887d5fbb50c0491605ca7
Nicolaj Figaw <figaw@hotmail.com>
2020-07-17 19:48:16 +0200

    ADD constant example

--- scripted-pipelines/library.groovy
+++ scripted-pipelines/library.groovy
@@ -1,8 +1,14 @@
 // Possible with the `GitHub Pipeline Plugin`
 @Library("github.com/figaw/jenkins-shared-libraries@test") _
 
+def myConstant = usingHelper.thisUsesAHelperFunction()
+
 node {
     stage("stage1"){
         echo usingHelper.thisUsesAHelperFunction()
+    }
+
+    stage("stage2"){
+        echo "It's even available here: $myConstant"
     }
 }
